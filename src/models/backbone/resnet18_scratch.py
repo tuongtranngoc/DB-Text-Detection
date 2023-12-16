4d60a06bd2fc20f6fcd9480fc583ecec417cc500
tuongtranngoc <ngoctuongwindy@gmail.com>
2023-12-16 22:12:49 +0700

    update: models

--- src/models/backbone/resnet18_scratch.py
+++ src/models/backbone/resnet18_scratch.py
@@ -30,10 +30,39 @@ class ResidualBlock(nn.Module):
         y = F.relu(y)
         return y
 
+
+class ResNet18(nn.Module):
+    def __init__(self):
+        cfg = [(2, 64), (2, 128), (2, 256), (2, 512)]
+        super(ResNet18, self).__init__()
+        self.net = nn.Sequential(self.b1())
+        for i, b in enumerate(cfg):
+            self.net.add_module(f'b{i+2}', self.block(*b, first_block=(i==0)))
+    
+    def b1(self): 
+        return nn.Sequential( 
+            nn.LazyConv2d(64, kernel_size=7, stride=2, padding=3), 
+            nn.LazyBatchNorm2d(), 
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+    def block(self, num_residuals, num_channels, first_block=False):
+        blk = []
+        for i in range(num_residuals):
+            if i == 0 or not first_block:
+                blk.append(ResidualBlock(num_channels, use_1x1_conv=True, strides=2))
+            else:
+                blk.append(ResidualBlock(num_channels))
+        return nn.Sequential(*blk)
+    
+    def forward(self, x):
+        return self.net(x)
+
+
 if __name__ == "__main__":
     from torchsummary import summary
-    residual = ResidualBlock(6, True, 2)
-    x = torch.randn(1, 3, 32, 32)
-    y = residual(x)
+    net = ResNet18()
+    x = torch.randn(1, 3, 640, 640)
+    y = net(x)
     import ipdb; ipdb.set_trace()    
 
