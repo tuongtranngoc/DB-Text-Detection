de1cc0468cb27951490a4bd27544155afbd34647
tuongtranngoc <ngoctuongwindy@gmail.com>
2023-12-23 10:30:28 +0700

    update: networks

--- src/models/diff_binarization.py
+++ src/models/diff_binarization.py
@@ -9,11 +9,25 @@ import torch.nn.functional as F
 
 from . import *
 
-class DifferentialBinarization(nn.Module):
+
+class DiffBinarization(nn.Module):
     def __init__(self):
         super().__init__()
         self.backbone = BackboneDB()
-        
+        self.neck = NeckDB(self.backbone.out_channels)
+        self.head = HeadDB(self.neck.out_channels)
 
     def forward(self, x):
-        pass
+        __, __, H, W  = x.size()
+        y = self.backbone(x)
+        y = self.neck(y)
+        y = self.head(y)
+        y = F.interpolate(y, size=(H, W), mode='bilinear', align_corners=True)
+        return y
+    
+
+if __name__ == "__main__":
+    DB = DiffBinarization()
+    x = torch.randn(2, 3, 640, 640)
+    y = DB(x)
+    import ipdb; ipdb.set_trace()
